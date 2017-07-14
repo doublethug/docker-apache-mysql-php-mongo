1bebbb6a9d349c2f17e841be55a79245f02bf7f9
ngoncharuk <nazar.goncharuk@inradius.io>
2017-07-14 18:37:12 +0200

    temp fixes

---
+++ set-env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export MYSQL_PASSWORD=123456;
+export MYSQL_ROOT_PASSWORD=123456;
+export DOCKER_DIRECTORY=/home/ngoncharuk/Projects/apps/
+export PHPMYADMIN_PORT=8082
+export CONF_COUCHDB_PORT=27017
+export MONGODB_DOCKERFILE=Dockerfile
+export MONGODB_PORT=27018
+export DOCKER_DATA_DIRECTORY=/home/ngoncharuk/Projects/apps/
+export PHP_DOCKERFILE=
+export HTTP_PORT=8087
+export HTTPS_PORT=8088
+export SSH_PORT=23
+export MYSQL_DATABASE=lingint
+export MYSQL_PORT=3906
+export PROJECT_DIRECTORY=/home/ngoncharuk/Projects/apps/lingint-cake/
